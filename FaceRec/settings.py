2a1ea8472a02d1119f1101ef94093fbeb6af6135
Boni Hazarika <75686621+Hazarika22@users.noreply.github.com>
2022-06-12 11:11:01 +0530

    Update settings.py

--- FaceRec/settings.py
+++ FaceRec/settings.py
@@ -105,7 +105,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'boni23hazarika@gmail.com'
+EMAIL_HOST_USER = 'E-Mail'
 EMAIL_HOST_PASSWORD = 'password'
 
 # Internationalization
